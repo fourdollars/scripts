7c974a35cc694059f6136207ac9bec107c0d2934
Shih-Yuan Lee (FourDollars) <fourdollars@gmail.com>
2013-08-08 13:29:14 +0800

    A script to enable debug symbols in Ubuntu.

---
+++ ubuntu/debug-symbols.sh
@@ -0,0 +1,32 @@
+#! /usr/bin/env bash
+# -*- coding: utf-8; indent-tabs-mode: nil; tab-width: 4; c-basic-offset: 4; -*-
+#
+# Copyright (C) 2013 Shih-Yuan Lee (FourDollars) <fourdollars@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+if ! grep ddebs.ubuntu.com /etc/apt/sources.list.d/ddebs.list >/dev/null 2>&1; then
+    echo "deb http://ddebs.ubuntu.com $(lsb_release -cs) main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list.d/ddebs.list
+    echo "deb http://ddebs.ubuntu.com $(lsb_release -cs)-updates main restricted universe multiverse
+deb http://ddebs.ubuntu.com $(lsb_release -cs)-security main restricted universe multiverse
+deb http://ddebs.ubuntu.com $(lsb_release -cs)-proposed main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list.d/ddebs.list
+fi
+
+if ! apt-key export 428D7C01 >/dev/null 2>&1; then
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 428D7C01
+fi
+
+sudo apt-get update
+
+# vim:fileencodings=utf-8:expandtab:tabstop=4:shiftwidth=4:softtabstop=4
