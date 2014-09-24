603a8cafcb6030e13b05f654611e297012ebf452
Shih-Yuan Lee (FourDollars) <fourdollars@gmail.com>
2014-09-24 21:18:04 +0800

    Add a script to automatically change the scaling factor for Unity desktop environment when using a HiDPI display.

---
+++ ubuntu/desktop-auto-scaliing.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8; indent-tabs-mode: nil; tab-width: 4; c-basic-offset: 4; -*-
+#
+# Copyright (C) 2014 Shih-Yuan Lee (FourDollars) <fourdollars@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+def check_hidpi_display():
+    from gi.repository import Gtk
+    import math
+    win = Gtk.OffscreenWindow()
+    screen = win.get_screen()
+    major = screen.get_primary_monitor()
+    diagonal = math.sqrt(screen.get_width() ** 2 + screen.get_height() ** 2)
+    diagonal_mm = math.sqrt(screen.get_monitor_width_mm(major) ** 2 + screen.get_monitor_height_mm(major) ** 2)
+    dpi = math.floor(diagonal * 25.4 / diagonal_mm)
+    if dpi / screen.get_resolution() >= 2:
+        return (True, screen.get_monitor_plug_name(major))
+    else:
+        return (False, screen.get_monitor_plug_name(major))
+
+def main():
+    (result, connector) = check_hidpi_display()
+
+    if not result:
+        return
+
+    from gi.repository import Gio, GLib
+    settings = Gio.Settings.new("com.ubuntu.user-interface")
+    outputs = settings.get_value("scale-factor").unpack()
+
+    outputs[connector] = 16
+    if connector == 'eDP1':
+        outputs['eDP-1-0'] = 16
+    elif connector == 'eDP-1-0':
+        outputs['eDP1'] = 16
+
+    settings.set_value("scale-factor", GLib.Variant('a{si}', outputs))
+
+if __name__ == '__main__':
+    main()
+
+# vim:fileencodings=utf-8:expandtab:tabstop=4:shiftwidth=4:softtabstop=4
