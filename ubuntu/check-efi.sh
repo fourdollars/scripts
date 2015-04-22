6961718671f16cbdfc2f81f2a008d5e2b0a18f57
Shih-Yuan Lee (FourDollars) <fourdollars@gmail.com>
2015-04-22 16:18:18 +0800

    Add a counter for check-efi.sh

--- ubuntu/check-efi.sh
+++ ubuntu/check-efi.sh
@@ -6,17 +6,24 @@ fi
 
 if [ ! -f ~/bin/check_efi ]; then
     cat > ~/bin/check_efi <<ENDLINE
-#!/bin/sh
+#!/bin/bash
+
+if [ -f ~/.check_efi_counter ]; then
+    . ~/.check_efi_counter
+else
+    COUNTER=1
+fi
 
 if [ ! -d /sys/firmware/efi ]; then
     zenity --info --title "No EFI System" --text "There is no /sys/firmware/efi"
 else
-    zenity --question --title "Would you like to stop?" --text "You have 5 seconds to decide." --timeout=5
-    RET=\$?
-    if [ \$RET != 0 ]; then
+    zenity --info --title "Would you like to stop?" --text "You have 5 seconds to decide. \$((COUNTER++)) time(s)" --ok-label Stop --timeout=5
+    if [ \$? != 0 ]; then
+        echo COUNTER=\$COUNTER > ~/.check_efi_counter
         dbus-send --system --print-reply --dest=org.freedesktop.login1 /org/freedesktop/login1 "org.freedesktop.login1.Manager.Reboot" boolean:true
-    elif [ \$RET = 0 ]; then
+    else
         rm ~/.config/autostart/check-efi.desktop
+        rm ~/.check_efi_counter
         rm ~/bin/check_efi
     fi
 fi
