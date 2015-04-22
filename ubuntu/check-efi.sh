119415b97fe002d9a4f94f4d9dcd341fe4ff2601
Shih-Yuan Lee (FourDollars) <fourdollars@gmail.com>
2015-04-22 15:18:03 +0800

    Check EFI system interface.

---
+++ ubuntu/check-efi.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+if [ ! -d ~/bin ]; then
+    mkdir ~/bin
+fi
+
+if [ ! -f ~/bin/check_efi ]; then
+    cat > ~/bin/check_efi <<ENDLINE
+#!/bin/sh
+
+if [ ! -d /sys/firmware/efi ]; then
+    zenity --info --title "No EFI System" --text "There is no /sys/firmware/efi"
+else
+    zenity --question --title "Would you like to stop?" --text "You have 5 seconds to decide." --timeout=5
+    if [ \$? != 0 ]; then
+        dbus-send --system --print-reply --dest=org.freedesktop.login1 /org/freedesktop/login1 "org.freedesktop.login1.Manager.Reboot" boolean:true
+    elif [ \$? = 0 ]; then
+        rm ~/.config/autostart/check-efi.desktop
+        rm ~/bin/check_efi
+    fi
+fi
+ENDLINE
+    chmod +x ~/bin/check_efi
+fi
+
+if [ ! -d ~/.config/autostart ]; then
+    mkdir ~/.config/autostart    
+fi
+
+if [ ! -f ~/.config/autostart/check-efi.desktop ]; then
+    cat > ~/.config/autostart/check-efi.desktop <<ENDLINE
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Check EFI
+Comment=Check EFI
+Exec=$HOME/bin/check_efi
+ENDLINE
+fi
