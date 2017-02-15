f7e9c65e1ebcbda78709e1f51a0675e8454b5ec5
Shih-Yuan Lee (FourDollars) <fourdollars@gmail.com>
2017-02-15 16:36:05 +0800

    Add the missing package.

--- ubuntu/download-kernel.sh
+++ ubuntu/download-kernel.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 VER=`uname -r` #4.4.0-62-generic
-apt download linux-signed-generic linux-signed-image-generic linux-headers-$VER linux-image-$VER linux-headers-generic linux-headers-$VER linux-generic linux-image-extra-$VER linux-signed-image-$VER linux-image-generic
+VERSION=`uname -r | cut -d '-' -f -2`
+apt download linux-signed-generic linux-signed-image-generic linux-headers-$VERSION linux-headers-$VER linux-image-$VER linux-headers-generic linux-headers-$VER linux-generic linux-image-extra-$VER linux-signed-image-$VER linux-image-generic
 
