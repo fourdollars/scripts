19364dadcb7e6d2b9380e673fd61a579edd8ba3b
Shih-Yuan Lee (FourDollars) <fourdollars@gmail.com>
2017-07-03 11:21:30 +0800

    Make a FreeDOS bootable device rapidly.

---
+++ mkfreedos.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 [device node, such as /dev/sdb]"
+    exit
+fi
+
+sudo apt install makebootfat --yes
+
+folder=`mktemp -u -d`
+
+mkdir -p $folder/fs-root
+cd $folder
+wget http://www.ibiblio.org/pub/micro/pc-stuff/freedos/files/distributions/1.0/pkgs/kernels.zip
+unzip kernels.zip source/ukernel/boot/fat*.bin
+cp -v source/ukernel/boot/fat*.bin .
+
+wget http://www.ibiblio.org/pub/micro/pc-stuff/freedos/files/distributions/1.0/pkgs/commandx.zip
+wget http://www.ibiblio.org/pub/micro/pc-stuff/freedos/files/distributions/1.0/pkgs/unstablx.zip
+unzip commandx.zip bin/command.com
+unzip unstablx.zip bin/kernel.sys
+cp -v bin/kernel.sys bin/command.com fs-root/
+
+sudo makebootfat -o $1 -L FreeDOS -E 255 -1 fat12.bin -2 fat16.bin -3 fat32lba.bin -m /usr/lib/syslinux/mbr/mbr.bin fs-root/
+cd
+rm -fr $folder
