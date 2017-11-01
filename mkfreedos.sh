9c923629e02fbd2d78012f508ced4340bc375a60
Shih-Yuan Lee (FourDollars) <fourdollars@gmail.com>
2017-11-01 12:12:22 +0800

    Add labeling when creating FreeDOS stick.

--- mkfreedos.sh
+++ mkfreedos.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
 if [ -z "$1" ]; then
-    echo "Usage: $0 [device node, such as /dev/sdb]"
+    echo "Usage: $0 [device node, such as /dev/sdb] [Label Name]"
     exit
+fi
+
+if [ ! -z "$2" ]; then
+    LABEL="$2"
+else
+    LABEL="FreeDOS"
 fi
 
 sudo apt install makebootfat --yes
@@ -21,6 +27,6 @@ unzip commandx.zip bin/command.com
 unzip unstablx.zip bin/kernel.sys
 cp -v bin/kernel.sys bin/command.com fs-root/
 
-sudo makebootfat -o $1 -L FreeDOS -E 255 -1 fat12.bin -2 fat16.bin -3 fat32lba.bin -m /usr/lib/syslinux/mbr/mbr.bin fs-root/
+sudo makebootfat -o $1 -L "$LABEL" -E 255 -1 fat12.bin -2 fat16.bin -3 fat32lba.bin -m /usr/lib/syslinux/mbr/mbr.bin fs-root/
 cd
 rm -fr $folder
