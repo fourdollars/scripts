b100c808e121488aed2390acb80ff2b870868412
Shih-Yuan Lee (FourDollars) <fourdollars@debian.org>
2019-07-31 17:07:04 +0800

    Checker shellcheck: check external sources by default.

--- vimplug.sh
+++ vimplug.sh
@@ -31,6 +31,7 @@ let g:syntastic_always_populate_loc_list = 1
 let g:syntastic_auto_loc_list = 1
 let g:syntastic_check_on_open = 1
 let g:syntastic_check_on_wq = 0
+let g:syntastic_sh_shellcheck_args = "-x"
 
 " My own preferred settings
 set hls
