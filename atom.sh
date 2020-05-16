4f1915d42a80aacabe7e4cbb4bf740b509454b67
Shih-Yuan Lee (FourDollars) <fourdollars@debian.org>
2020-05-16 21:06:01 +0800

    Install atom.

--- atom.sh
+++ atom.sh
@@ -4,3 +4,5 @@ wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | gpg --dearmor > atom
 sudo install -o root -g root -m 644 atom.gpg /usr/share/keyrings/
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/atom.gpg] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
 rm atom.gpg
+sudo apt update
+sudo apt install atom
