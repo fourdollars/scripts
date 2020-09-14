1e9252928483e8a95eee225ab22596987e524c5b
Shih-Yuan Lee (FourDollars) <fourdollars@debian.org>
2020-09-14 12:54:43 +0800

    Make pep8 ignore W503 in vim.

--- vimplug.sh
+++ vimplug.sh
@@ -53,7 +53,7 @@ let g:syntastic_check_on_open = 1
 let g:syntastic_check_on_wq = 0
 let g:syntastic_sh_shellcheck_args = "-x"
 let g:syntastic_python_checkers = ['pep8']
-let g:syntastic_python_pep8_args = "--ignore=E501"
+let g:syntastic_python_pep8_args = "--ignore=E501,W503"
 
 " My own preferred settings
 set expandtab
