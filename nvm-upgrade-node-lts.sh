8e24a2748f0fdbd3ea7e1e19a8011de4be3d0a72
Shih-Yuan Lee (FourDollars) <fourdollars@debian.org>
2021-08-15 20:23:23 +0800

    Update nvm-upgrade-node-lts.sh for the latest nodejs lts.

--- nvm-upgrade-node-lts.sh
+++ nvm-upgrade-node-lts.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-export NVM_DIR="$HOME/.nvm"
+export NVM_DIR="$HOME/.config/nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 if grep dreamhost /etc/resolv.conf >/dev/null 2>&1; then
     CURRENT=$(readlink "$HOME/bin/node" | cut -d '/' -f 7)
